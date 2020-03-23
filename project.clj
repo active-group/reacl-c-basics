855ff367e694002b52fc3d34fb8148e999f15184
David Frese <david.frese@active-group.de>
2020-03-23 13:42:29 +0100

    Release 0.7.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.active-group/reacl-c-basics "0.7.3-SNAPSHOT"
+(defproject de.active-group/reacl-c-basics "0.7.3"
   :description "Library with utilities often useful when writing web applications with Reacl-C."
   
   :url "http://github.com/active-group/reacl-c-basics"
