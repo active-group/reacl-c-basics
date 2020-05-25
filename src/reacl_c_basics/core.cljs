545c3d4d8dc998cf700a5bae97eda69d0470440f
David Frese <david.frese@active-group.de>
2020-05-25 19:28:03 +0200

    Add timer and animation-frame utils.

--- src/reacl_c_basics/core.cljs
+++ src/reacl_c_basics/core.cljs
@@ -1,5 +1,6 @@
 (ns reacl-c-basics.core
-  (:require [reacl-c.dom :as dom]))
+  (:require [reacl-c.dom :as dom]
+            [reacl-c.core :as c]))
 
 (defn ^:no-doc split-dom-attrs [args]
   (if (and (not-empty args)
@@ -11,3 +12,29 @@
   (fn [& args]
     (let [[attrs children] (split-dom-attrs args)]
       (apply f attrs children))))
+
+(c/defn-subscription animation-frame
+  "Subscription to the browser's animation frames, emitting the timestamps as actions."
+  deliver! []
+  (let [id (js/window.requestAnimationFrame (fn [timestamp]
+                                              (deliver! timestamp)))]
+    (fn []
+      (js/window.cancelAnimationFrame id))))
+
+(c/defn-subscription timeout
+  "Subscription to a timer, emitting `true` as an action once, after they given number of milliseconds."
+  deliver! [ms]
+  (let [id (js/window.setTimeout (fn []
+                                   (deliver! true))
+                                 ms)]
+    (fn []
+      (js/window.clearTimeout id))))
+
+(c/defn-subscription interval
+  "Subscription to a timer, emitting `true` as an action every given milliseconds."
+  deliver! [ms]
+  (let [id (js/window.setInterval (fn []
+                                    (deliver! true))
+                                  ms)]
+    (fn []
+      (js/window.clearInterval id))))
