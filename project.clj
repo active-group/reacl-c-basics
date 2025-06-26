f9fbf0cf782122e6251783f0720985b33ff6f581
David Frese <david.frese@active-group.de>
2025-06-26 13:51:41 +0200

    Begin 0.12 series.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.active-group/reacl-c-basics "0.11.10"
+(defproject de.active-group/reacl-c-basics "0.12.0-SNAPSHOT"
   :description "Library with utilities often useful when writing web applications with Reacl-C."
   
   :url "http://github.com/active-group/reacl-c-basics"
@@ -8,7 +8,7 @@
 
   :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
                  [org.clojure/clojurescript "1.10.773" :scope "provided"]
-                 [de.active-group/reacl-c "0.11.7"]
+                 [de.active-group/reacl-c "0.12.1"]
                  [de.active-group/active-clojure "0.41.0"]
                  [venantius/accountant "0.2.5"]
                  [clout "2.2.1"]
@@ -21,8 +21,10 @@
             [lein-auto "0.1.3"]]
 
   :profiles {:dev {:dependencies [[de.active-group/cljs-async "2.0.0"]
-                                  [de.active-group/reacl-c-testing "0.1.0"]]}
-             :shadow [:dev {:dependencies [[thheller/shadow-cljs "2.11.7"]
+                                  [de.active-group/reacl-c-testing "0.2.0-SNAPSHOT"]]}
+             :shadow [:dev {:dependencies [[org.clojure/clojure "1.12.0"]
+                                           [org.clojure/clojurescript "1.12.38"]
+                                           [thheller/shadow-cljs "3.0.4"]
                                            [binaryage/devtools "1.0.2"]]
                             :source-paths ["src" "test" "examples"]
                             :resource-paths ["target" "resources"]}]
